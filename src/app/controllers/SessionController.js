a3893a33cff92cd93b6b908f922650c39f94fe20
hog099 <oliveiraginelli@gmail.com>
2019-09-23 23:16:08 -0300

    initial api start to use

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,6 +3,7 @@ import * as Yup from 'yup';
 import authConfig from '../../config/auth';
 
 import User from '../models/User';
+import File from '../models/File';
 
 class SessionController {
 
@@ -19,7 +20,16 @@ class SessionController {
 
         const {email, password} = req.body;
 
-        const user = await User.findOne({where: {email}});
+        const user = await User.findOne({
+            where: {email},
+            include: [
+                {
+                    model: File,
+                    as: 'avatar',
+                    attributes: ['id','path','url'],
+                }
+            ]
+        });
 
         if(!user){
             return res.status(401).json({error: 'Usuário não Existe na Base de Dados.'});
@@ -29,13 +39,15 @@ class SessionController {
             return res.status(401).json({error: 'A Senha Não Confere com a do Usuário.'});
         }
 
-        const {id, name} = user;
+        const {id, name, avatar, provider} = user;
 
         return res.json({
             user:{
                 id,
                 name,
-                email
+                email,
+                provider,
+                avatar,
             },
             token: jwt.sign( 
                 {id}, 
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
 
@@ -57,9 +58,22 @@ class UserController {
             return res.status(401).json({error: "Senha Não Confere."});
         }
 
-        const {id, name, provider} = await user.update(req.body);
+        
+        //Atualiza Dados do perfil
+        await user.update(req.body);
+        
 
-        return res.json({ id, name, email, provider });
+        const {id, name, avatar} = await User.findByPk(req.userId, {
+            include: [
+                {
+                    model: File,
+                    as: 'avatar',
+                    attributes: ['id','path','url']
+                }
+            ]
+        });
+
+        return res.json({ id, name, email, avatar });
     }
 
 
